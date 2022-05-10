1235f2c196f24c628e1f6ab83ea62770df272465
sfjdx1144 <sfjdx1144@live.com>
2022-05-10 20:32:24 +0800

    fix

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@
 
 from setuptools import setup, find_packages
 import os
-# python setup.py sdist 打包成tar.gz的形式
-# python setup.py bdist_wheel  打包成wheel格式
+
 try:
     with open(os.path.join(os.path.abspath(os.path.dirname("__file__")),"README.md")) as f:
         long_description=f.read()
@@ -21,7 +20,7 @@ setup(
     author="Fujun Sun",
     author_email="sfjdx1144@live.com",
     packages=find_packages(),
-    install_requires=['requests','argparse','colorama'],
+    install_requires=['argparse','colorama'],
     platforms="any",
     license="GNU GPLv3 Licence",
     python_requires=">=3.4.0",
